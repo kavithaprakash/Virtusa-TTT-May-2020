be22cc6709aae4c50a4a078787d2a0fd27b5d257
kavithaprakash <65166937+kavithaprakash@users.noreply.github.com>
2020-05-19 16:17:44 +0530

    Add files via upload

---
+++ Day 1/sample/src/main/java/org/ttt/example/sample/App.java
@@ -0,0 +1,18 @@
+package org.ttt.example.sample;
+
+import com.virtusa.day1.CalculatorDemo.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+   Calculator c=new Calculator();
+  c.add();
+  
+     
+    }
+}
